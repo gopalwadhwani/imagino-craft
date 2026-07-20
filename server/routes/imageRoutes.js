322d03c9d5157c13ce294c6ad738494e6dc3b204
gopal wadhwani <gopalwadhwani435@gmail.com>
2026-07-20 21:53:09 +0530

    implemented generate img feature

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,8 +1,10 @@
+// routes/imageRoutes.js
 import express from 'express'
 import { generateImage } from '../controllers/imageController.js'
+import userAuth from '../middlewares/auth.js'
 
 const imageRouter = express.Router()
 
-imageRouter.post('/generate', generateImage)
+imageRouter.post('/generate', userAuth, generateImage)
 
 export default imageRouter
