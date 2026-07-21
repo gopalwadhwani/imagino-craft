aae8138e9f060973b817de6f8719e00446e14844
gopal wadhwani <gopalwadhwani435@gmail.com>
2026-07-21 19:37:20 +0530

    result input overflow issue fixed

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -47,7 +47,7 @@ const Result = () => {
           <input
             onChange={e => setInput(e.target.value)} value={input}
             type="text" placeholder='Describe what you want to generate'
-            className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color' />
+            className='flex-1 min-w-0 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color' />
 
           <button type='submit'
             className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'
@@ -56,12 +56,12 @@ const Result = () => {
       }
 
       {isImageLoaded &&
-        <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full'>
-          <p onClick={() => { setIsImageLoaded(false) }}
-            className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Generate Another</p>
-          <a href={image} download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>Download</a>
-        </div>
-      }
+  <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full'>
+    <p onClick={() => { setIsImageLoaded(false); setInput('') }}
+      className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Generate Another</p>
+    <a href={image} download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>Download</a>
+  </div>
+}
 
     </form>
   )
