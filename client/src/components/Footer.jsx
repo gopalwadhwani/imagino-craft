46a1eef7485abd07c00eea87e9afd8936d051ac9
gopal wadhwani <gopalwadhwani435@gmail.com>
2026-07-25 21:48:14 +0530

    completed mobile functionaliies

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,22 +3,22 @@ import { motion } from "motion/react"
 
 const Footer = () => {
     return (
-        <motion.div className='flex items-center justify-between gap-4 py-3 mt-20'
+        <motion.div className='flex items-center justify-between gap-4 py-3 mt-20 px-4'
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1 }}
             viewport={{ once: true }}>
 
-            <img src={assets.logo} alt="" width={150} />
+            <img src={assets.logo} alt="" className='w-24 sm:w-32 md:w-40' />
 
-            <p className='flex-1 broder-1 border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>
+            <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>
                 Copyright @GreatStack.dev | All right reserved.
             </p>
 
-            <div className='flex gap-2.5'>
-                <img src={assets.facebook_icon} alt="" width={35} />
-                <img src={assets.twitter_icon} alt="" width={35} />
-                <img src={assets.instagram_icon} alt="" width={35} />
+            <div className='flex gap-2 sm:gap-2.5'>
+                <img src={assets.facebook_icon} alt="" className='w-6 sm:w-8' />
+                <img src={assets.twitter_icon} alt="" className='w-6 sm:w-8' />
+                <img src={assets.instagram_icon} alt="" className='w-6 sm:w-8' />
             </div>
 
         </motion.div>
